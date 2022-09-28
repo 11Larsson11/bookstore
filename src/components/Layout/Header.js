e828e66896de891a815f9124a602252c48495f4d
11Larsson11 <1richardlarsson1@gmail.com>
2022-09-28 10:47:53 +0200

    A React training website for Udemy Class

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import bookImage from "../../assets/books.jpg";
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Books</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={bookImage} alt='A library of great books!' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
